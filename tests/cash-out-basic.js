b6e6888d1311a0407376e8bfeb54c10658fd6a63
Josh Harvey <josh@lamassu.is>
2016-12-27 14:54:47 +0200

    WIP

--- tests/cash-out-basic.js
+++ tests/cash-out-basic.js
@@ -11,7 +11,12 @@ module.exports = {
       .pause(100)
       .assert.containsText('.choose_fiat_state .sums .fiat .fiat-amount', '15')
       .click('#cash-out-button')
-      .pause(5000)
+      .waitForElementVisible('#qr-code-deposit', 10000)
+      .waitForElementVisible('.pending_deposit_state', 5000)
+      .waitForElementVisible('.dispensing_state', 5000)
+      .waitForElementVisible('.fiat_complete_state', 5000)
+      .click('body')
+      .waitForElementVisible('#cash-in', 10000)
       .end()
   }
 }
